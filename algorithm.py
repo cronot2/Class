80f60ed9803d575e8568f9771ce7ef7a85ed5c33
cronot2 <frbarazarte@gmail.com>
2026-06-09 19:12:50 -0400

    I changed the json format and how every function work

--- algorithm.py
+++ algorithm.py
@@ -1,77 +1,121 @@
-
+import random
 import json
+from turtle import title
+
+def read_json():
+    try:
+        with open('books.json', 'r', encoding='UTF-8') as file:
+            data = json.load(file)
+            books = data.get("books", [])
+            return books
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def write_json(books_list):
+    data_to_save = {
+        "books": books_list
+    }
+    with open('books.json', 'w', encoding='UTF-8') as file:
+        json.dump(data_to_save, file, indent= 4, ensure_ascii=False)
 
 def search_book(user_book):
-    with open('books.json', 'r', encoding='UTF-8') as file:
-        
-        books = json.load(file)
-        normal_book = [book.strip().lower() for book in books]
-        
-        if user_book in normal_book:
-            print(f'The book, \"{user_book.capitalize()}\" is in the library\n')
-        else:
-            print(f'The book, \"{user_book.capitalize()}\" is not in the library\n')
-            file.closed
+    books = read_json()
+    found_books = None
+
+    if user_book.isdigit():
+        id_books = int(user_book)
+        for book in books:
+            if book['id'] == id_books:
+                found_books = book
+                break
+    else:
+        for book in books:
+            if book['title'].lower() == user_book:
+                found_books = book
+                break
+
+    if found_books:
+        print(f'Title:{found_books["title"].title()}, is in the library.')
+    else:
+        print('The book is not in the library')
+            
 
 def add_book(user_book):
-    with open('books.json', 'r', encoding='UTF-8') as file:
-        data = json.load(file)
-        data.append(user_book)
+    books = read_json()
 
-    with open('books.json', 'w', encoding='UTF-8') as file:
-        file.write(json.dumps(data))
-        file.closed
-    print('the book was added succesfully.\n')
+    new_id = random.randint(10000, 99999)
 
-def edit_book(new_book, old_book):
-    with open('books.json', 'r', encoding='UTF-8') as file:
-        data = json.load(file)
+    new_book = {
+        "id":new_id,
+        "title":user_book
+    }
 
-        if old_book in data:
-            i_book = data.index(old_book)
-            data[i_book] = new_book
-            with open('books.json', 'w', encoding='UTF-8') as file:
-                file.write(json.dumps(data))
-                file.closed
-            print('The book was edited succesfully.\n')
-        else:
-            print('Are you sure that is the book?, check the name.\n')
+    books.append(new_book)
+
+    write_json(books)
+    print(f'The book was added successfully, ID:{new_id}  Title:{user_book.title()}')
+
+def edit_book(user_book, old_book):
+    books = read_json()
+    found = False
+    
+    if old_book.isdigit():
+        id_book = int(old_book)
+        for book in books:
+            if book["id"] == id_book:
+                book["title"] = user_book
+                found = True
+                break
+    else:
+        for book in books:
+            if (old_book).lower().strip() in book["title"].lower():
+                book["title"] = user_book
+                found = True
+                break
+    
+    if found:
+        write_json(books)
+        print(f'\n{user_book.title()} was added successfully')
+    else:
+        print('\nCheck the id or the title')
+            
 
 def delete_book(user_book):
-    with open('books.json', 'r', encoding='UTF-8') as file:
-        data = json.load(file)
-        data_n = [dat.lower() for dat in data]
-
-    if user_book in data_n:
-        data_n.remove(user_book)
-        with open('books.json', 'w', encoding='UTF-8') as file:
-            file.write(json.dumps(data_n))
-            file.closed
-        print('The book was deleted succesfully.')
+    books = read_json()
+    initial_length = len(books)
+    
+    if user_book.isdigit():
+        id_book = int(user_book)
+        books = [book for book in books if book["id"] != id_book]
     else:
-        print('Check the book again.')
+        books = [book for book in books if book['title'] != user_book]
+        
+    if len(books) < initial_length:
+        write_json(books)
+        print(f'\n{user_book} was deleted successfully')
+    else:
+        print('\nCheck the id or title, please')
 
 def show_library():
-    with open('books.json', 'r', encoding='UTF-8') as file:
-        books = json.load(file)
-
-        for book in books:
-            print(book.title())
+    books = read_json()
+    
+    for book in books:
+        print(f'\nID:{book["id"]} Title:{book['title'].title()}')
 
 def menu():
     while True:
         user_option = None
-        print('''\n***Welcom to Library***
+        print('''\n***Welcome to Library***
 1.Search a book.
 2.Show books.
-3.Add a New book.
+3.Add a new book.
 4.Edit a book.
 5.Delete a book.
-6.Salir''')
+6.Exit''')
         try:
             user_option = int(input('What do you want to do?:'))
         except ValueError:
-            print('\nInsert a integer number.')
+            print('\nPlease enter an integer number.')
 
         if user_option == 1:
             
@@ -86,9 +130,9 @@ def menu():
             
         elif user_option == 4:
             
-            old_book=input('\nWhat book do you want to edit?: ').strip()
-            new_book=input('What is the new name for the book?: ').strip().lower()
-            edit_book(new_book=new_book, old_book=old_book)
+            old_book=input('\nWhat book do you want to edit?: ')
+            new_book=input('What is the new name for the book?: ').lower().strip()
+            edit_book(user_book=new_book, old_book=old_book)
 
         elif user_option == 5:
 
